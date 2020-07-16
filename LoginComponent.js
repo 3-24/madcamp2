3143b7a740218d9a6889c1a3242b7769bc467bc4
3-24 <dev.youngseok@gmail.com>
2020-07-17 02:18:15 +0900

    Build basic login UI

---
+++ LoginComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TouchableOpacity, View, StyleSheet, TextInput, Text } from 'react-native';
+
+class LoginComponent extends React.Component {
+    render(){
+        return <View style={styles.container}>
+            <TextInput style={styles.inputbox} placeholder="ID"/>
+            <TextInput style={styles.inputbox} placeholder="password"/>
+            <TouchableOpacity style={styles.button} >
+            <Text>LOGIN!</Text>
+            </TouchableOpacity>
+            </View>
+            ;
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        alignItems: 'center',
+        flex:1,
+        justifyContent: 'center',
+        alignItems:'stretch',
+        padding: 10,
+    },
+    inputbox : {
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingLeft: 10,
+    },
+    button : {
+        backgroundColor: "#DDDDDD",
+        alignItems: 'center',
+        padding: 10
+    }
+})
+
+export default LoginComponent;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import LoginCompoennt from './LoginComponent';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => LoginCompoennt);
