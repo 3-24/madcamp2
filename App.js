48c1573b3b13972f7cf951f86c20a30870191fed
Youngseok Choe <yschoelife@gmail.com>
2020-07-16 21:32:46 +0900

    Apply basic icons and color to tab

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function FriendScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+      <Text>Friends!</Text>
     </View>
   );
 }
@@ -14,7 +15,7 @@ function FriendScreen() {
 function FeedScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Feeds!</Text>
     </View>
   );
 }
@@ -22,7 +23,7 @@ function FeedScreen() {
 function ProfileScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Profile!</Text>
     </View>
   );
 }
@@ -32,7 +33,28 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === '친구') {
+              iconName = focused ? "people-outline" : "people"
+            } else if (route.name === '피드') {
+              iconName = focused ? 'grid-outline' : 'grid';
+            } else if (route.name === "프로필") {
+              iconName = focused ? 'happy-outline' : 'happy';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'navy',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="친구" component={FriendScreen} />
         <Tab.Screen name="피드" component={FeedScreen} />
         <Tab.Screen name="프로필" component={ProfileScreen} />
