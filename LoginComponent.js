3315f89356f7477593009eb8c9defe14953ab240
3-24 <dev.youngseok@gmail.com>
2020-07-18 02:55:03 +0900

    Change LoginComponent type from function to component

--- LoginComponent.js
+++ LoginComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TouchableOpacity, View, StyleSheet, TextInput, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,23 +11,7 @@ import {
   statusCodes,
 } from '@react-native-community/google-signin';
 
-_signIn = async () => {
-  try {
-    await GoogleSignin.hasPlayServices();
-    const userInfo = await GoogleSignin.signIn();
-    this.setState({ userInfo });
-  } catch (error) {
-    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-      // user cancelled the login flow
-    } else if (error.code === statusCodes.IN_PROGRESS) {
-      // operation (e.g. sign in) is in progress already
-    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-      // play services not available or outdated
-    } else {
-      // some other error happened
-    }
-  }
-};
+
 
 isSignedIn = async () => {
   const isSignedIn = await GoogleSignin.isSignedIn();
@@ -50,23 +34,51 @@ signOut = async () => {
 };
 
 
-function LoginComponent({navigation}) {
-    GoogleSignin.configure({
-    webClientId: "690743107079-mtcoqcq0maoh8kl4gun0es6pi7hcaiil.apps.googleusercontent.com", // client ID of type WEB for your server(needed to verify user ID and offline access)
-    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-    accountName: '', // [Android] specifies an account name on the device that should be used
-        });
+class LoginComponent extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        pushData: [],
+        loggedIn: false
+      }
+    }
+
+    componentDidMount() {
+      GoogleSignin.configure({
+        webClientId: "690743107079-mtcoqcq0maoh8kl4gun0es6pi7hcaiil.apps.googleusercontent.com", // client ID of type WEB for your server(needed to verify user ID and offline access)
+        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+        forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+        accountName: '', // [Android] specifies an account name on the device that should be used
+            });
+    }
+
+    _signIn = async () => {
+      try {
+        await GoogleSignin.hasPlayServices();
+        const userInfo = await GoogleSignin.signIn();
+        this.setState({ userInfo: userInfo, loggedIn: true });
+      } catch (error) {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          // operation (f.e. sign in) is in progress already
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          // play services not available or outdated
+        } else {
+          // some other error happened
+        }
+      }
+    };
     
-    
+    render(){
     return(
             <View style={styles.container}>
             <TextInput style={styles.inputbox} placeholder="ID"/>
             <TextInput style={styles.inputbox} placeholder="password"/>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Main')} >
+            <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Main')} >
             <Text>LOGIN!</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Register')} >
+            <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Register')} >
             <Text>REGISTER</Text>
             </TouchableOpacity>
             <GoogleSigninButton
@@ -77,6 +89,7 @@ function LoginComponent({navigation}) {
               disabled={this.state.isSigninInProgress} />
             </View>
     );
+    }
 }
 
 const Stack = createStackNavigator();
